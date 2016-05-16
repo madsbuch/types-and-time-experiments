5c6237dc9f7c49516efb0b85c5fbd28ec9a1f1b5
Mads Buch <madspbuch@gmail.com>
2016-05-16 22:42:08 +0200

    initial proposal for the language is done

--- Language.hs
+++ Language.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 
 module Language where
@@ -14,21 +15,22 @@ type family Add (n :: Nat) (m :: Nat) :: Nat
 type instance Add Z      m = m
 type instance Add (S m)  n = S (Add m n)
 
--- Secret Type
-data STypes where
-    BBool :: Boolean -> STypes
-    BInt  :: Integer -> STypes
-    BChar :: Char    -> STypes
+-- Type level Multiplication
+type family Mult (n :: Nat) (m :: Nat) :: Nat
+type instance Mult Z     m = Z
+type instance Mult (S m) n = (Add n (Mult m n))
 
--- Public type
-data PType where
-    Nil  :: PType
-    Cons :: STypes -> PType -> PType
+-- List type, Size is the only thing we leak
+data List a (size :: Nat) where
+    Nill :: List a Z
+    Cons :: a -> List a m -> List a (S m)
 
 -- Define the lagnuage
 data CoreLang t (s :: Nat) where
     -- Literals
     LBool :: Bool -> CoreLang Bool Z
+    Lint  :: Int  -> CoreLang Int Z 
+    LList :: List a s -> CoreLang (List a s) Z
 
     -- Booleans
     And  :: CoreLang Bool m -> CoreLang Bool n -> CoreLang Bool (S (Add m n))
@@ -38,11 +40,18 @@ data CoreLang t (s :: Nat) where
     -- Integer
     Plus  :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
     Minus :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
-    Mult  :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
+    Time  :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
     Div   :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
 
-    -- Control Structures
-    If   :: BBool -> CoreLang m -> CoreLang m -> CoreLang (S m)
+    -- List operations
+    Map  :: CoreLang (List (CoreLang a m) s) n -> (CoreLang a m -> CoreLang b n) -> CoreLang (List (CoreLang b n) s) (Add n s)
+    Fold :: (CoreLang a m1 -> CoreLang a m2 -> CoreLang a m3) -- function
+        -> CoreLang a n0 -- Neutral element
+        -> List (CoreLang a n1) s
+        -> CoreLang a (Add (Mult s m3) n0)
+
+    -- Misc
+    If   :: CoreLang Bool m1 -> CoreLang a m2 -> CoreLang a m2 -> CoreLang a (S (Add m1 m2))
 
 --An interpreter
 interpret :: CoreLang t m -> t
