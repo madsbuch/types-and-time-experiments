12c0582d10e4cf61dc7f99df7902f7fa8d137a54
Mads Buch <madspbuch@gmail.com>
2016-05-30 13:52:02 +0200

    soooo many type errors

--- Language.hs
+++ Language.hs
@@ -26,66 +26,82 @@ data List a (size :: Nat) where
     Nill :: List a Z
     (:::) :: a -> List a m -> List a (S m)
 
+data TypePack a where
+    B :: Bool     -> TypePack Bool
+    L :: List a s -> TypePack (List a s)
+    I :: Int      -> TypePack Int
+
 -- Define the lagnuage
 data CoreLang t (s :: Nat) where
     -- Literals
-    LBool :: Bool -> CoreLang Bool Z
-    LInt  :: Int  -> CoreLang Int Z 
-    LList :: List a s -> CoreLang (List a s) s
+    Lit   :: TypePack a -> CoreLang (TypePack a) Z
 
     -- Booleans
-    And  :: CoreLang Bool m -> CoreLang Bool n -> CoreLang Bool (S (Add m n))
-    Or   :: CoreLang Bool m -> CoreLang Bool n -> CoreLang Bool (S (Add m n))
-    Not  :: CoreLang Bool m -> CoreLang Bool (S m)
+    --And  :: CoreLang Bool m -> CoreLang Bool n -> CoreLang Bool (S (Add m n))
+    Or   :: CoreLang (TypePack Bool) m -> CoreLang (TypePack Bool) n -> CoreLang (TypePack Bool) (S (Add m n))
+    --Not  :: CoreLang Bool m -> CoreLang Bool (S m)
 
     -- Integer
-    Plus  :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
-    Minus :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
-    Time  :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
-    Div   :: CoreLang Int m -> CoreLang Int n -> CoreLang Int (S (Add m n))
-    IEq   :: CoreLang Int m -> CoreLang Int n -> CoreLang Bool (S (Add m n))
+    --Plus  :: CoreLang Int m -> CoreLang Int n -> CoreLang Int  (S (Add m n))
+    --Minus :: CoreLang Int m -> CoreLang Int n -> CoreLang Int  (S (Add m n))
+    --Time  :: CoreLang Int m -> CoreLang Int n -> CoreLang Int  (S (Add m n))
+    --Div   :: CoreLang Int m -> CoreLang Int n -> CoreLang Int  (S (Add m n))
+    --IEq   :: CoreLang Int m -> CoreLang Int n -> CoreLang Bool (S (Add m n))
 
     -- List operations
-    Map  :: CoreLang (List (CoreLang a m) s) n 
-        -> (CoreLang a m -> CoreLang b n) 
-        -> CoreLang (List (CoreLang b n) s) (Mult n s)
-    Fold :: (CoreLang a Z -> CoreLang a Z -> CoreLang a (S Z)) -- function, only constant operations time on literals
-        -> CoreLang a Z -- Neutral element, only constants
-        -> CoreLang (List (CoreLang a Z) s) s --list to fold over, s-sized list of constant values
-        -> CoreLang a s
+    Map  :: CoreLang (List (TypePack a) s) n
+        -> (CoreLang a Z -> CoreLang b fTime) 
+        -> CoreLang (List (TypePack b) s) (Add n (Mult fTime s))
+--    Fold :: (CoreLang a Z -> CoreLang a Z -> CoreLang a m) -- function, only constant operations time on literals
+--        -> CoreLang a n0 -- Neutral element, only constants
+--        -> CoreLang (List a s) t --list to fold over, s-sized list of constant values
+--        -> CoreLang a (Add t (Add (Mult s m) n0))
 
     -- Misc - actually, the relevant stuff
     -- Conditional
-    If   :: CoreLang Bool m1 -> CoreLang a m2 -> CoreLang a m2 -> CoreLang a (S (Add m1 m2))
+    --If   :: CoreLang Bool m1 -> CoreLang a m2 -> CoreLang a m2 -> CoreLang a (S (Add m1 m2))
     -- parallell computations
-    -- Par ...
+    -- Par  
 
 instance Show a => Show (List a s) where
     show (x ::: xs) = (show x) ++ " ::: " ++ (show xs)
     show (Nill)     = "Nill" 
 
+instance Show a => Show (TypePack a) where
+    show (B b) = "(B " ++ show b ++ ")"
+    show (I i) = "(I " ++ show i ++ ")"
+    show (L l) = "(L " ++ show l ++ ")"
+
 instance Show t => Show (CoreLang t s) where
-    show (LBool b) = show b
-    show (LInt i)  = show i
-    show (LList l) = show l
-    show (Fold f i l) = "(Fold f " ++ show i ++ " " ++ show l ++ ")"
+    show (Lit l) = (show l)
+--    show (Fold f i l) = "(Fold f " ++ show i ++ " " ++ show l ++ ")"
+
+
+typeUnpack :: TypePack a -> a
+typeUnpack (I i)    = i
+typeUnpack (B b)    = b
+typeUnpack (L l)    = l
 
 --An interpreter
 interpret :: CoreLang t m -> t
-interpret (LBool a)    = a
-interpret (LInt i)     = i
-interpret (LList l)    = l
-interpret (Fold f z (LList l)) = interpret $ unfold f z l
+interpret (Lit l)  = l
+
+interpret (Map list f) = Lit $ doMap (interpret list)
   where
-    unfold f z Nill         = z
-    unfold f z (x ::: xs)   = f x (unfold f z xs)
+    doMap :: List (TypePack a) s -> (CoreLang (TypePack a) Z -> CoreLang (TypePack b) fTime) -> List (TypePack b) s 
+    doMap Nill f          = Nill
+    --doMap (x ::: xs)    = (interpret (f (Lit x))) ::: (doMap xs)
 
 {- Some Applications -}
 
 -- Secure login, User
-userList = LList (LInt 133 ::: LInt 3434 ::: LInt 23234 ::: LInt 3434 ::: Nill)
-userCheckTrue  = (LInt 133)
-userCheckFalse = (LInt 323)
+
+-- ordinary Haskell
+--checkUser us ul = foldl (||) False (map (==us) ul)
+
+--userList = LList (LInt 133 ::: LInt 3434 ::: LInt 23234 ::: LInt 3434 ::: Nill)
+--userCheckTrue  = (LInt 133)
+--userCheckFalse = (LInt 323)
 
 
 {-
@@ -96,19 +112,19 @@ This first example generates a program which can check if a user exists in a lis
 --    -> CoreLang (List (CoreLang Int Z) s) (S (Add Z Z)) 
 --    -> CoreLang Bool (Add (Mult s (S Z)) Z)
 
-elementEquals x xs = (Map xs (\y -> (IEq y x)))
+--elementEquals x xs = (Map xs (\y -> (IEq y x)))
 
 doOr :: CoreLang Bool m -> CoreLang Bool n -> CoreLang Bool (S (Add m n))
 doOr a b = (Or a b)
 
 false :: CoreLang Bool Z
-false = (LBool False)
+false = (Lit $ B False)
 
 
-listBools = LList (LBool False ::: LBool False ::: LBool True ::: LBool False ::: Nill)
+listBools = Lit $ L (B False ::: B False ::: B True ::: B False ::: Nill)
 
-foldOr :: CoreLang (List (CoreLang Bool Z) s) s -> CoreLang Bool s
-foldOr xs = (Fold doOr false xs)
+--foldOr :: CoreLang (List Bool s) s -> CoreLang Bool s
+--foldOr xs = (Fold doOr false xs)
 
 --    Fold :: (CoreLang a m1 -> CoreLang a m2 -> CoreLang a (S (Add m1 m2))) -- function, only constant operations time
 --        -> CoreLang a n0 -- Neutral element
