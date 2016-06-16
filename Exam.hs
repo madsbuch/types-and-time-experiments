0a62562f1729956402bb63f52b7ac8f7cfd18a39
Mads Buch <madspbuch@gmail.com>
2016-06-16 13:23:16 +0200

    update

--- Exam.hs
+++ Exam.hs
@@ -12,26 +12,48 @@ import UserAuthentication
 import Auction
 import GHC.Exts (Constraint)
 
+-- introducing the language
+
+{- Some Data -}
+intList = L (I 1 ::: I 2 ::: I 3 ::: I 4 ::: Nill )
+plusFold l = Fold (Lit l) (Lit (I 0)) (\a b -> (Plus a b))
+
+-- User authentication
+
+
+
+-- Auction
+
+-- Constant time multiplication
+
+-- A litle easter egg: Our investigations allow that we can limit how many ticks
+-- programs can take. This is done as follows:
+
+-- Value level naturals
 data SNat (a :: Nat) where
   SZ :: SNat Z
   SS :: SNat a -> SNat (S a)
 
-class IsLeq (a :: Nat) where
+-- A class, not important..
+class ConstraintSatisfied (a :: Nat) where
+instance ConstraintSatisfied Z where
+instance ConstraintSatisfied (S t) where
 
-instance IsLeq Z where
-instance IsLeq (S t) where
-
+-- Alright, this is tricky. We _only_ create a constraint iff n <= m. This results
+-- in an ugly compile time error if it is not the case. 
 type family Leq (n :: Nat) (m :: Nat) :: Constraint
-type instance Leq Z     m      = IsLeq m
+type instance Leq Z     m      = ConstraintSatisfied m
 type instance Leq (S m) (S n)  = (Leq m n)
+
+-- A bound that ensures that they are within 50% of each others.
+type family Bound (a :: Nat) (m :: Nat) :: Constraint
+type instance a b  = (Leq a (Mult b (S(S(Z)))))
+type instance a b  = (Leq b (Mult a (S(S(Z)))))
 
 limitRuntimeInterpreter :: Leq t limit => SNat limit -> CoreLang a t -> (a, Integer)
 limitRuntimeInterpreter _ exp = interpret exp
 
-{- Some Data -}
-intList = Lit $ L (I 1 ::: I 2 ::: I 3 ::: I 4 ::: Nill )
-
-plusFold l = Fold l (Lit (I 0)) (\a b -> (Plus a b))
+boundExeTimeInterpreter :: 
 
 {- Play with compile til runtime constraints -}
 
